8f20a74d8f3c7bbb1f4a0073ac3e90d9f99e0dda
Kissa Leon Denis <kldenis2001@gmail.com>
2021-10-16 16:38:05 +0300

    New files

---
+++ making_my_own_header_files/calculate_age.c
@@ -0,0 +1,4 @@
+int calculate_age(int birth_year, int current_year){
+	//function to calculate age
+	return current_year - birth_year;
+}
---
+++ making_my_own_header_files/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "myfile.h" //preprocessor  reference to my header file
+
+int main(){
+	printf("Age is %d",calculate_age(2001,2021));
+	return 0;
+}
